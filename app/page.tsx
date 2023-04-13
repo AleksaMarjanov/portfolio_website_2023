7c0b43c9f1f50d4a9cd379f168c233441c56a53b
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-04-13 15:18:07 -0500

    ADHD Fast Reader paragraph test

--- app/page.tsx
+++ app/page.tsx
@@ -14,17 +14,18 @@ export default function Home() {
         <main className="flex items-center text-black w-full min-h-screen">
             <div className="pt-0">
                 <Layout >
-                    <div className='flex items-center justify-between w-full'>
-                        <div className="w-1/2">
+                    <div className='flex flex-col md:flex-row items-center justify-between w-full'>
+                        <div className="md:w-1/2">
                             <Image
                                 src={profilePic}
                                 priority
                                 alt="AleksaMarjanov"
                                 className="w-full h-auto rounded-full" />
                         </div>
-                        <div>
-                            <AnimatedText text=" Software Developer | Web Developer " className="" />
-                            <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. </p>
+                        <div className="md:w-1/2 flex flex-col items-center self-center">
+                            <AnimatedText text="Software Developer | Web Developer " className="!text-6xl !text-left" />
+                            <span><b>Exper</b>ienced <b>fro</b>nt-end <b>dev</b>eloper, <b>Brin</b>ging <b>yo</b>ur
+                                <b> digi</b>tal <b>dre</b>ams <b>to</b> <b>li</b>fe <b>wi</b>th <b>visi</b>onary <b>fro</b>nt-end <b>deve</b>lopment.</span>
                         </div>
                     </div>
                 </Layout>'
--- components/AnimatedText.tsx
+++ components/AnimatedText.tsx
@@ -1,23 +1,67 @@
+"use client";
+
 import React from 'react'
+import { motion } from 'framer-motion';
+import { staggerContainer } from '@/utils/motion';
 
 type Props = {
     text: string;
     className: string;
 }
 
+const quote = {
+    intial: {
+        opacity: 0,
+    },
+    animate: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            staggerChildren: 0.08,
+        }
+    }
+}
+
+const singleWord = {
+    intial: {
+        opacity: 0,
+        y: 50,
+    },
+    animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1,
+        }
+    }
+}
+
+
+
 const AnimatedText = ({ text, className = "" }: Props) => {
     return (
-        <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
-            <h1 className={` ${className} inline-block w-full text-black font-bold capitalize text-8xl`}>
+        <motion.div
+            variants={staggerContainer}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true, amount: 0.25 }}
+            className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
+            <motion.h1
+                variants={quote}
+                initial="hidden"
+                whileInView="show"
+                className={` ${className} inline-block w-full text-black font-bold capitalize `}>
                 {
                     text.split(" ").map((word, index) =>
-                        <span key={word + '-' + index} >
+                        <motion.span
+                            variants={singleWord}
+                            key={word + '-' + index} className="inline-block">
                             {word}&nbsp;
-                        </span>
+                        </motion.span>
                     )
                 }
-            </h1>
-        </div>
+            </motion.h1>
+        </motion.div >
     )
 }
 
