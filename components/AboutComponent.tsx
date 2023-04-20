575fb73b6e878aea3997ff74a6df9b06cc3d8bf7
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-04-20 16:27:23 -0500

    ..

--- components/AboutComponent.tsx
+++ components/AboutComponent.tsx
@@ -67,8 +67,8 @@ const AboutComponent = () => {
                 <div className="pt-16">
                     <Layout>
                         <AnimatedText text="Meet the Passionate Problem Solver Behind the Code" className="mb-8 md:mb-16 !text-4xl md:!text-6xl !text-center" />
-                        <div className='grid w-full gap-y-8 md:gap-y-0 grid-cols-8 gap-4 sm:gap-8 md:gap-16'>
-                            <div className="w-full lg:col-span-3 flex flex-col items-start justify-start order-2 md:order-1 col-span-8">
+                        <div className='grid w-full gap-y-8 md:gap-y-4 lg:gap-y-0 grid-cols-8 gap-4 sm:gap-8 md:gap-16'>
+                            <div className="w-full md:col-span-4 lg:col-span-3 flex flex-col items-start justify-start order-2 lg:order-1 col-span-8">
                                 <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>About Me</h2>
                                 {about?.map((item: About) => (
                                     <motion.div
@@ -100,10 +100,10 @@ const AboutComponent = () => {
                                 ))}
                             </div>
                             {/* max-w-[540px] md:max-w-[300px] */}
-                            <div className='col-span-8 md:col-span-4 lg:col-span-3 relative 
-                                items-center justify-center h-max rounded-2xl border-2 border-solid border-[#F7AB0A] bg-light p-8 order-1 md:order-2'>
+                            <div className='md:max-w-[400px] col-span-8  md:col-span-4 lg:col-span-3 relative 
+                                items-center justify-center h-max rounded-2xl border-2 border-solid border-[#F7AB0A] bg-light p-8 order-1 lg:order-2'>
 
-                                <div className='absolute top-0 -right-3 -z-10 w-[100.5%] h-[103%] rounded-[2.5rem] rounded-br-3xl  bg-[#F7AB0A]' />
+                                <div className='absolute top-0 -right-3 -z-10 w-[105%] h-[103%] rounded-[2.5rem] rounded-br-3xl  bg-[#F7AB0A]' />
 
                                 {/* <div className='absolute top-0 -right-3 -z-10 w-[105%] h-[103%] rounded-[2rem] bg-[#F7AB0A] dark:bg-light' /> */}
                                 <Image src={profilePic}
@@ -116,7 +116,7 @@ const AboutComponent = () => {
                                 />
                             </div>
 
-                            <div className='col-span-8 flex-row lg:col-span-2 flex lg:flex-col items-center lg:items-end justify-between order-3 '>
+                            <div className='col-span-8 flex-row lg:col-span-2 flex lg:flex-col items-center justify-between order-3 '>
 
                                 <div>
                                     <span className="inline-block text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
