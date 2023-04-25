44c43064fe5eadcb6e6221a306dd324b85c8dcdb
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-04-25 18:38:12 -0500

    ..

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,9 +10,9 @@ export const metadata = {
     title: 'Projects Page'
 }
 
-// type ProjectType = {
-//     project: Project[]
-// }
+type ProjectType = {
+    project: Projects
+}
 
 const query = groq`
 *[_type == 'project']
@@ -23,6 +23,9 @@ const Projects = async () => {
 
     const projects = await client.fetch(query)
 
+    console.log({ projects });
+
+
     return (
         <main className="w-full p-2 md:p-0 mb-16 flex flex-col items-center justify-center">
             {/* <div className="pt-16"> */}
@@ -30,8 +33,8 @@ const Projects = async () => {
                 <AnimatedText text="Creative Portfolio" className='mb-8 !text-4xl md:!text-7xl lg:!text-8xl lg:mb-16' />
 
                 <div className="grid grid-cols-12 gap-24 gap-x-0 xs:gap-x-8 sm:gap-x-16 md:gap-y-24  xl:gap-y-32" >
-                    <div className="col-span-12">
-                        {/* <FeaturedProject type="Featured Project" title="Marjanov Design Solutions" summary="Web dev company" img={project} link="https://marjanovdesignsolutions.com" github="https://github.com/marjanovdesignsolutions" /> */}
+                    < div className="col-span-12" >
+                        <FeaturedProject type='Business Website' title="Marjanov Design Solutions" summary="Single page website made for my website development company showcasing portfolio and some information about services provided" img={project} link="https://marjanovdesignsolutions.com" github="https://github.com/AleksaMarjanov/marjanovdesignsolutions" />
                     </div>
                     {projects?.map((project: Projects) => (
                         <div key={project._id} className="col-span-12 lg:col-span-6">
