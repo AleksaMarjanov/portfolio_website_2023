ce5d329f7cdf9030548f2d88f212ad3bbc7cb0e5
AleksaMarjanov <aleksa.marjanov@outlook.com>
2023-04-27 16:25:28 -0500

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import { Footer, Navbar } from '@/components';
 import './globals.css'
 import { Montserrat } from 'next/font/google'
-import { AnimatePresence } from 'framer-motion';
 
 const montserrat = Montserrat({
     weight: ["400", "700"],
@@ -11,25 +8,24 @@ const montserrat = Montserrat({
     variable: "--font-montserrat",
 });
 
-// export const metadata = {
-//     title: 'Software Developer - Aleksa Marjanov',
-//     description: 'Web Developer | Software Developer, create unique designs',
-// }
+export const metadata = {
+    title: 'Software Developer - Aleksa Marjanov',
+    description: 'Web Developer | Software Developer, create unique designs',
+}
 
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+
     return (
         <html lang="en">
             <body className={`${montserrat.className} bg-light w-full min-h-screen `} >
                 <Navbar />
-                <AnimatePresence>
-                    {children}
-                </AnimatePresence>
+                {children}
                 <Footer />
             </body>
-        </html>
+        </html >
     )
 }
